daed19adcf2e24e91e8c4a2d8dbd1b8e47342f61
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-10-23 21:42:44 -0400

    Create atv 01 - listas.py

---
+++ Modulo 8 - Listas/atv 01 - listas.py
@@ -0,0 +1,15 @@
+# Crie um programa para fazer uma consulta de estoque. 
+# O usuário do programa deve inserir o nome do produto e, caso ele não exista na lista, ele deve ser avisado. 
+#Caso exista, o programa deve dizer a quantidade de unidades em estoque do produto
+
+
+produtos = ['tv', 'celular', 'tablet', 'mouse', 'teclado', 'geladeira', 'forno']
+estoque = [100, 150, 100, 120, 70, 180, 80]
+
+busca = input("Insira o nome do produto: ")
+
+if busca in produtos:
+    i = produtos.index(busca)
+    print("Você tem {} unidades do produto {} em estoque" .format(estoque[i], produtos[i]))
+else:
+    print("Produto não encontrado")
